4201c63f8b3ac6c2c332a33cde5db5542d8c4c70
hjs0925 <hjsn0925@naver.com>
2025-04-01 00:26:18 +0900

    마무리

--- game/Assets/GameControl.cs
+++ game/Assets/GameControl.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
+using System.Linq;
 
 public class GameControl : MonoBehaviour
 {
@@ -17,72 +18,174 @@ public class GameControl : MonoBehaviour
     public TextMeshProUGUI scoreText;
     public GameObject gameOver;
     public TextMeshProUGUI gameOverText;
-    public GameObject timeSlider;
     public TextMeshProUGUI addScoreText;
     public RectTransform hintBox;
     public TextMeshProUGUI modeText;
-
+    public TimeManager TM;
     [Header("Game Settings")]
     public int horizontalLength;
     public int verticalLength;
     public float comboTime;
-
     [Header("Only for Read")]
     public int nothing;
     public int score = 0;
     public float comboDelta = 0f;
     public int comboCnt;
-
     public GameMode currentMode = GameMode.Add;
-
     private Vector3 zeroPos;
     private Vector2 startpos = Vector2.zero;
     private Rect selectRect = new Rect();
-
     private List<AppleMeta> applesList;
     private List<AppleMeta> selectList;
-
-    private TimeManager TM;
-
     private int[,] mapApple;
     public List<Vector4> hints;
     private List<Vector2Int> answerCoors;
 
     private void Awake()
-    {
-        zeroPos = apple.transform.position;
-        applesList = new List<AppleMeta>();
-        selectList = new List<AppleMeta>();
-        mapApple = new int[verticalLength, horizontalLength];
-        hints = new List<Vector4>();
+{
+    zeroPos = apple.transform.position;
+    applesList = new List<AppleMeta>();
+    selectList = new List<AppleMeta>();
+    mapApple = new int[verticalLength, horizontalLength];
+    hints = new List<Vector4>();
 
+    // Subtract 모드일 때 가로 길이 홀수면 1 줄이기
+    if (currentMode == GameMode.Subtract && horizontalLength % 2 != 0)
+    {
+        horizontalLength -= 1;
+        mapApple = new int[verticalLength, horizontalLength]; // 배열 다시 초기화
+    }
+
+    InitBoard();
+}
+
+    private void InitBoard()
+{
+    applesList.Clear();
+    selectList.Clear();
+    mapApple = new int[verticalLength, horizontalLength];
+
+    if (currentMode == GameMode.Subtract)
+{
+    // 1. 숫자쌍 미리 생성
+    List<int> numbers = new List<int>();
+    int pairCount = (horizontalLength * verticalLength) / 2;
+
+    for (int i = 0; i < pairCount; i++)
+    {
+        int baseNum = Random.Range(11, 20);
+        int secondNum = baseNum - 10;
+
+        // 순서 랜덤하게
+        if (Random.value < 0.5f)
+        {
+            numbers.Add(baseNum);
+            numbers.Add(secondNum);
+        }
+        else
+        {
+            numbers.Add(secondNum);
+            numbers.Add(baseNum);
+        }
+    }
+
+    // 2. 좌표 전부 뽑아서 셔플
+    List<Vector2Int> coords = new List<Vector2Int>();
+    for (int i = 0; i < verticalLength; i++)
+    {
+        for (int j = 0; j < horizontalLength; j++)
+        {
+            coords.Add(new Vector2Int(i, j));
+        }
+    }
+
+    // 좌표 랜덤 섞기
+    for (int i = 0; i < coords.Count; i++)
+    {
+        Vector2Int temp = coords[i];
+        int randIndex = Random.Range(i, coords.Count);
+        coords[i] = coords[randIndex];
+        coords[randIndex] = temp;
+    }
+
+    // 3. 숫자들 보드에 랜덤하게 배치
+    for (int i = 0; i < coords.Count && i < numbers.Count; i++)
+    {
+        Vector2Int coor = coords[i];
+        Vector3 newPos = new Vector3(zeroPos.x + coor.y, zeroPos.y + coor.x, zeroPos.z);
+        GameObject newApple = Instantiate(apple, newPos, Quaternion.identity);
+
+        AppleMeta am = newApple.GetComponent<AppleMeta>();
+        int num = numbers[i];
+
+        am.number = num;
+        am.coor = coor;
+        am.isOn = true;
+
+        newApple.name = $"Apple ({coor.x}, {coor.y})";
+        newApple.SetActive(true);
+
+        mapApple[coor.x, coor.y] = num;
+        applesList.Add(am);
+    }
+
+    // 4. 남는 칸이 홀수면 빈 칸 추가
+    if (numbers.Count < coords.Count)
+    {
+        Vector2Int coor = coords[coords.Count - 1];
+        Vector3 newPos = new Vector3(zeroPos.x + coor.y, zeroPos.y + coor.x, zeroPos.z);
+        GameObject newApple = Instantiate(apple, newPos, Quaternion.identity);
+
+        AppleMeta am = newApple.GetComponent<AppleMeta>();
+        am.number = Random.Range(1, 10);
+        am.coor = coor;
+        am.isOn = false; // 선택 안 되도록
+
+        newApple.name = $"Apple ({coor.x}, {coor.y})";
+        newApple.SetActive(true);
+
+        mapApple[coor.x, coor.y] = am.number;
+        applesList.Add(am);
+    }
+}
+
+    else // Add 모드
+    {
         for (int i = 0; i < verticalLength; i++)
         {
             for (int j = 0; j < horizontalLength; j++)
             {
                 Vector3 newPos = new Vector3(zeroPos.x + j, zeroPos.y + i, zeroPos.z);
                 GameObject newApple = Instantiate(apple, newPos, Quaternion.identity);
-                AppleMeta _am = newApple.GetComponent<AppleMeta>();
-                newApple.name = string.Format("Apple ({0}, {1})", i, j);
-                int _number = Random.Range(1, 10);
-                _am.number = _number;
-                _am.coor = new Vector2Int(i, j);
-                mapApple[i, j] = _number;
-                applesList.Add(_am);
+
+                AppleMeta am = newApple.GetComponent<AppleMeta>();
+                int randNum = Random.Range(1, 10);
+
+                am.number = randNum;
+                am.coor = new Vector2Int(i, j);
+                am.isOn = true;
+
+                newApple.name = $"Apple ({i}, {j})";
+                newApple.SetActive(true);
+
+                mapApple[i, j] = randNum;
+                applesList.Add(am);
             }
         }
     }
 
+    FindHints(); // 힌트 갱신
+}
+
     void Start()
     {
         Screen.SetResolution(1920, 1080, true);
+
         foreach (AppleMeta _apple in applesList)
         {
             _apple.gameObject.SetActive(true);
             _apple.isOn = true;
         }
-
-        TM = timeSlider.GetComponent<TimeManager>();
         gameOver.SetActive(false);
 
         FindHints();
@@ -115,45 +218,50 @@ public class GameControl : MonoBehaviour
         gameOverText.text = string.Format("Game Over\n\nYour Score: {0}", score);
     }
 
-    private void calculateAnswer()
+private void calculateAnswer()
+{
+    if (!Input.GetMouseButtonUp(0)) return;
+
+    int cnt = selectList.Count;
+    int result = 0;
+
+    if (currentMode == GameMode.Add)
     {
-        if (!Input.GetMouseButtonUp(0)) return;
-
-        int result = 0;
-        int cnt = 0;
-        foreach (AppleMeta selectedApple in selectList)
+        foreach (AppleMeta apple in selectList)
         {
-            answerCoors.Add(selectedApple.coor);
-            cnt += 1;
+            result += apple.number;
         }
-
-        if (currentMode == GameMode.Add)
+        if (result == 10)
         {
-            foreach (AppleMeta selectedApple in selectList)
-            {
-                result += selectedApple.number;
-            }
-            if (result == 10)
-            {
-                OnCorrectAnswer(cnt);
-            }
+            OnCorrectAnswer(cnt);
         }
-        else if (currentMode == GameMode.Subtract && selectList.Count > 0)
-        {
-            result = selectList[0].number;
-            for (int i = 1; i < selectList.Count; i++)
-            {
-                result -= selectList[i].number;
-            }
-            if (result == 10)
-            {
-                OnCorrectAnswer(cnt);
-            }
-        }
-
-        selectList.Clear();
-        answerCoors.Clear();
     }
+    else if (currentMode == GameMode.Subtract && selectList.Count > 1)
+    {
+        AppleMeta maxApple = selectList[0];
+        foreach (AppleMeta apple in selectList)
+        {
+            if (apple.number > maxApple.number)
+                maxApple = apple;
+        }
+
+        result = maxApple.number;
+        foreach (AppleMeta apple in selectList)
+        {
+            if (apple != maxApple)
+                result -= apple.number;
+        }
+
+        if (result == 0)
+        {
+            OnCorrectAnswer(cnt);
+        }
+    }
+
+    selectList.Clear();
+    answerCoors.Clear();
+}
+
 
     private void OnCorrectAnswer(int cnt)
     {
@@ -205,15 +313,23 @@ public class GameControl : MonoBehaviour
     }
 
     public void OnToggleModeButton()
+{
+    // 기존 사과 제거
+    foreach (AppleMeta apple in applesList)
     {
-        if (currentMode == GameMode.Add)
-            currentMode = GameMode.Subtract;
-        else
-            currentMode = GameMode.Add;
-
-        UpdateModeUI();
-        FindHints();
+        if (apple != null)
+            Destroy(apple.gameObject);
     }
+
+    applesList.Clear();
+
+    // 모드 전환
+    currentMode = (currentMode == GameMode.Add) ? GameMode.Subtract : GameMode.Add;
+
+    // 새로운 보드 세팅
+    InitBoard();
+    UpdateModeUI();
+}
 
     private void UpdateModeUI()
     {
@@ -253,8 +369,11 @@ public class GameControl : MonoBehaviour
         selectBox.offsetMin = selectRect.min;
         selectBox.offsetMax = selectRect.max;
 
-        collideBox.offsetMin = Camera.main.ScreenToWorldPoint(selectRect.min);
-        collideBox.offsetMax = Camera.main.ScreenToWorldPoint(selectRect.max);
+        Vector2 worldMin = Camera.main.ScreenToWorldPoint(selectRect.min);
+        Vector2 worldMax = Camera.main.ScreenToWorldPoint(selectRect.max);
+        collideBox.position = (worldMin + worldMax) / 2f;
+        collideBox.sizeDelta = worldMax - worldMin;
+
     }
 
     private void SelectSystem()
@@ -293,76 +412,87 @@ public class GameControl : MonoBehaviour
     }
 
     private void FindHints()
+{
+    answerCoors = new List<Vector2Int>();
+    hints.Clear();
+    Vector4 vector = new Vector4();
+
+    for (int x = 0; x < verticalLength; x++)
     {
-        answerCoors = new List<Vector2Int>();
-        hints.Clear();
-        Vector4 vector = new Vector4();
-        int sum = 0;
-
-        for (int x = 0; x < verticalLength; x++)
+        for (int y = 0; y < horizontalLength; y++)
         {
-            for (int y = 0; y < horizontalLength; y++)
+            // 가로 방향
+            for (int length = 2; length <= horizontalLength - y; length++)
             {
-                sum = 0;
-
-                for (int k = y; k < horizontalLength; k++)
+                List<int> values = new List<int>();
+                for (int k = 0; k < length; k++)
                 {
-                    if (currentMode == GameMode.Add)
-                    {
-                        sum += mapApple[x, k];
-                        if (sum > 10) break;
-                        if (sum == 10)
-                        {
-                            vector.Set(x, y, x, k);
-                            hints.Add(vector);
-                        }
-                    }
-                    else if (currentMode == GameMode.Subtract)
-                    {
-                        int result = mapApple[x, y];
-                        for (int j = y + 1; j <= k; j++)
-                        {
-                            result -= mapApple[x, j];
-                        }
-                        if (result == 10)
-                        {
-                            vector.Set(x, y, x, k);
-                            hints.Add(vector);
-                        }
-                    }
+                    values.Add(mapApple[x, y + k]);
                 }
 
-                sum = 0;
-
-                for (int k = x; k < verticalLength; k++)
+                bool isValid = false;
+                if (currentMode == GameMode.Add)
                 {
-                    if (currentMode == GameMode.Add)
-                    {
-                        sum += mapApple[k, y];
-                        if (sum > 10) break;
-                        if (sum == 10)
-                        {
-                            vector.Set(x, y, k, y);
-                            hints.Add(vector);
-                        }
-                    }
-                    else if (currentMode == GameMode.Subtract)
-                    {
-                        int result = mapApple[x, y];
-                        for (int j = x + 1; j <= k; j++)
-                        {
-                            result -= mapApple[j, y];
-                        }
-                        if (result == 10)
-                        {
-                            vector.Set(x, y, k, y);
-                            hints.Add(vector);
-                        }
-                    }
+                    isValid = values.Sum() == 10;
+                }
+                else if (currentMode == GameMode.Subtract)
+                {
+                    isValid = IsValidSubtractCombo(values);
+                }
+
+                if (isValid)
+                {
+                    vector.Set(x, y, x, y + length - 1);
+                    hints.Add(vector);
+                }
+            }
+
+            // 세로 방향
+            for (int length = 2; length <= verticalLength - x; length++)
+            {
+                List<int> values = new List<int>();
+                for (int k = 0; k < length; k++)
+                {
+                    values.Add(mapApple[x + k, y]);
+                }
+
+                bool isValid = false;
+                if (currentMode == GameMode.Add)
+                {
+                    isValid = values.Sum() == 10;
+                }
+                else if (currentMode == GameMode.Subtract)
+                {
+                    isValid = IsValidSubtractCombo(values);
+                }
+
+                if (isValid)
+                {
+                    vector.Set(x, y, x + length - 1, y);
+                    hints.Add(vector);
                 }
             }
         }
     }
+}
+    private bool IsValidSubtractCombo(List<int> values)
+{
+    if (values.Count < 2)
+        return false;
+
+    int max = values.Max();
+    values.Remove(max);
+    values.Sort((a, b) => b.CompareTo(a)); // 내림차순
+
+    int result = max;
+    foreach (int val in values)
+    {
+        result -= val;
+    }
+
+    return result == 0;
+}
+
 
     public void OnResetButton()
     {
--- game/Assets/TimeManager.cs
+++ game/Assets/TimeManager.cs
@@ -1,22 +1,33 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class TimeManager : MonoBehaviour
 {
-    public float fSliderBarTime;
+    public float fSliderBarTime = 120f; // 전체 시간
     public bool isEnd = false;
+
     private float maxBarTime;
 
-    public Image clockImage; // 시계 이미지 연결
+    public Image clockImage; // 시계 UI 이미지
+    public TextMeshProUGUI timeText; // 남은 시간 숫자로 표시 (선택)
 
     void Start()
     {
         maxBarTime = fSliderBarTime;
+
+        // 초기값 설정
+        if (clockImage != null)
+            clockImage.fillAmount = 1f;
+
+        if (timeText != null)
+            timeText.text = Mathf.CeilToInt(fSliderBarTime).ToString();
     }
 
     void Update()
     {
         fSliderBarTime -= Time.deltaTime;
+
         if (fSliderBarTime < 0)
         {
             isEnd = true;
@@ -24,6 +35,11 @@ public class TimeManager : MonoBehaviour
         }
 
         float ratio = fSliderBarTime / maxBarTime;
-        clockImage.fillAmount = ratio;
+
+        if (clockImage != null)
+            clockImage.fillAmount = ratio;
+
+        if (timeText != null)
+            timeText.text = Mathf.CeilToInt(fSliderBarTime).ToString();
     }
 }
